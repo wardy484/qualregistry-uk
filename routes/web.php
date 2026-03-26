59ade40b3939348f2b632abe4ad4b59a92d138ed
Kim <wardy484@gmail.com>
2026-03-26 04:39:19 +0000

    style: fix import ordering in routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\InstitutionBrowseController;
 use App\Http\Controllers\IngestionOpsController;
+use App\Http\Controllers\InstitutionBrowseController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
